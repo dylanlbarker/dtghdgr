9f78cf466444ba389812a17192375a5b2b4fd8fe
Dylan Barker <dylanlbarker@icloud.com>
2022-09-21 13:02:02 +1000

    Initial Commit

---
+++ dtghdgr/dtghdgrApp.swift
@@ -0,0 +1,17 @@
+//
+//  dtghdgrApp.swift
+//  dtghdgr
+//
+//  Created by Dylan Barker on 21/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct dtghdgrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
